608f95a3faca83e73e1f618081c64f501548c6fd
bridgekat <zhanrong.qiao21@imperial.ac.uk>
2022-02-04 01:22:16 +0000

    Heapsort!

--- src/functional_programming/graphs_and_algorithms/binary_heap.lean
+++ src/functional_programming/graphs_and_algorithms/binary_heap.lean
@@ -15,13 +15,17 @@ namespace min_heap
 
   section
     variables {α : Type}
-    variables [inhabited α] [linear_order α]
+    variables [inhabited α] [linear_order α] [has_to_string α]
 
     notation `∅` := min_heap.nil
 
     def all_nodes : min_heap α → list α
     | ∅            := []
     | (node l a r) := (all_nodes l) ++ [a] ++ (all_nodes r)
+
+    def print : min_heap α → string
+    | ∅            := ""
+    | (node l a r) := "(" ++ (print l) ++ " " ++ (to_string a) ++ " " ++ (print r) ++ ")"
 
     def is_empty : min_heap α → bool
     | ∅ := tt
@@ -47,11 +51,16 @@ namespace min_heap
     | (node l a ∅) := node (remove_last l) a ∅
     | (node l a r) := node l a (remove_last r)
 
-    lemma size_remove_last : ∀ h : min_heap α, h.remove_last.size = h.size - 1
-    | ∅ := rfl
-    | (node ∅ a ∅) := rfl
-    | (node l a ∅) := by { sorry }
-    | (node l a r) := by { sorry }
+    lemma size_remove_last (h : min_heap α) : h.remove_last.size = h.size - 1 := by
+    { induction h with l a r hl hr,
+      refl,
+      cases l with l₁ a₁ r₁,
+      { cases r with l₂ a₂ r₂,
+        { simp [remove_last, size] },
+        { simp [remove_last, size, hr], ring_nf } },
+      { cases r with l₂ a₂ r₂,
+        { simp [remove_last, size, hl], ring_nf },
+        { simp [remove_last, size, hl, hr], ring_nf } } }
 
     def fix_min_heap : min_heap α → min_heap α
     | ∅ := ∅
@@ -63,31 +72,41 @@ namespace min_heap
       if a₂ < a then node ∅ a₂ (fix_min_heap $ node l₂ a r₂)
       else           node ∅ a (node l₂ a₂ r₂)
     | (node (node l₁ a₁ r₁) a (node l₂ a₂ r₂)) :=
-      if      a₁ < a then node (fix_min_heap $ node l₁ a r₁) a₁ (node l₂ a₂ r₂)
-      else if a₂ < a then node (node l₁ a₁ r₁) a₂ (fix_min_heap $ node l₂ a r₂)
-      else                node (node l₁ a₁ r₁) a (node l₂ a₂ r₂)
+      if a₁ < a₂ then
+      ( if a₁ < a then node (fix_min_heap $ node l₁ a r₁) a₁ (node l₂ a₂ r₂)
+        else           node (node l₁ a₁ r₁) a (node l₂ a₂ r₂) )
+      else
+      ( if a₂ < a then node (node l₁ a₁ r₁) a₂ (fix_min_heap $ node l₂ a r₂)
+        else           node (node l₁ a₁ r₁) a (node l₂ a₂ r₂) )
 
     lemma size_fix_min_heap : ∀ h : min_heap α, h.fix_min_heap.size = h.size
-    | ∅ := by { unfold fix_min_heap }
-    | (node ∅ a ∅) := by { unfold fix_min_heap }
+    | ∅ := by { simp [fix_min_heap, size] }
+    | (node ∅ a ∅) := by { simp [fix_min_heap, size] }
     | (node (node l₁ a₁ r₁) a ∅) := by
-      { unfold fix_min_heap, sorry }
-    | (node ∅ a (node l₂ a₂ r₂)) := by sorry
-    | (node (node l₁ a₁ r₁) a (node l₂ a₂ r₂)) := by sorry
+      { simp [fix_min_heap, size], split_ifs;
+        simp [fix_min_heap, size, size_fix_min_heap (node l₁ a r₁)] }
+    | (node ∅ a (node l₂ a₂ r₂)) := by
+      { simp [fix_min_heap, size], split_ifs;
+        simp [fix_min_heap, size, size_fix_min_heap (node l₂ a r₂)] }
+    | (node (node l₁ a₁ r₁) a (node l₂ a₂ r₂)) := by
+      { simp [fix_min_heap, size], split_ifs;
+        simp [fix_min_heap, size],
+        { simp [fix_min_heap, size, size_fix_min_heap (node l₁ a r₁)] },
+        { simp [fix_min_heap, size, size_fix_min_heap (node l₂ a r₂)] } }
 
-    def remove_min (h : min_heap α) : min_heap α :=
-      let ⟨last, h'⟩ := (get_last h, remove_last h) in
-        match h' with
-        | ∅            := node ∅ last ∅
-        | (node l a r) := fix_min_heap $ node l last r
-        end
+    def remove_min : min_heap α → min_heap α
+    | ∅              := ∅
+    | h@(node l a r) := fix_min_heap ∘ remove_last $ node l (get_last h) r
 
-    lemma size_remove_min : ∀ h : min_heap α, h.remove_min.size = h.size - 1 := sorry
+    lemma size_remove_min : ∀ h : min_heap α, h.remove_min.size = h.size - 1 := by
+    { intros h, induction h with l a r hl hr,
+      { simp [size, remove_min, get_last, remove_last] },
+      { simp [size, remove_min, get_last, remove_last, size_remove_last, size_fix_min_heap] } }
 
     def init_min_heap : min_heap α → min_heap α
     | ∅ := ∅
     | (node l a r) := fix_min_heap $ node (init_min_heap l) a (init_min_heap r)
-    
+
     open nat
 
     -- Greatest integer power of 2 less or equal than n
@@ -130,15 +149,14 @@ namespace min_heap
     | [] := ∅
     | [a] := node ∅ a ∅
     | ls'@(a :: (b :: ls)) :=
-      let l := gpow2 (length ls' - 1) in
-      have h₁ : l - 1 < ls'.length := by
-      { have := gpow2_le (length (a :: b :: ls) - 1),
-        replace this : l - 1 ≤ length ls := sub_le_sub_right' this 1,
-        apply lt_succ_of_le,
-        exact le_trans this (le_succ (length ls)) },
-      have h₂ : length ls' - gpow2 (length ls' - 1) < length ls' := by
-      { suffices : 0 < gpow2 (length ls + 1), { exact nat.sub_lt (pos_of_gt h₁) this },
-        exact zero_lt_gpow2 (length ls + 1) (zero_lt_succ _) },
+      let l := gpow2 (length ls') in
+      have h₁ : gpow2 (length ls') - 1 < length ls' := by
+      { have := gpow2_le (length (a :: b :: ls)),
+        cases gpow2 (a :: b :: ls).length,
+        { simp }, { exact this } },
+      have h₂ : length ls' - gpow2 (length ls') < length ls' := by
+      { suffices : 0 < gpow2 (length ls + 2), { refine nat.sub_lt _ this, simp },
+        exact zero_lt_gpow2 (length ls + 2) (zero_lt_succ _) },
       node (from_list $ take (l - 1) ls') (ls'.nth_le (l - 1) h₁) (from_list $ drop l ls')
     using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf length⟩]}
 
@@ -150,18 +168,22 @@ end min_heap
 -- Heapsort
 section
   variables {α : Type}
-  variables [inhabited α] [linear_order α]
+  variables [inhabited α] [linear_order α] [has_to_string α]
 
   def extract_heap : min_heap α → list α
   | min_heap.nil := []
   | hp@(min_heap.node l a r) :=
-    let ⟨min, h⟩ := (hp.get_min, hp.remove_min) in
-      have h.size < (l.node a r).size := by { sorry },
-        min :: extract_heap h
+    have hp.remove_min.size < (l.node a r).size := by
+    { have := min_heap.size_remove_min (min_heap.node l a r),
+      simp [min_heap.size] at *,
+      rw this, simp },
+    hp.get_min :: extract_heap hp.remove_min
   using_well_founded {rel_tac := λ _ _, `[exact ⟨_, measure_wf min_heap.size⟩]}
 
   def heapsort (ls : list α) : list α :=
-    extract_heap (min_heap.from_list ls)
+    extract_heap ∘ min_heap.init_min_heap ∘ min_heap.from_list $ ls
+
+  #eval heapsort $ [1, 4, 7, 4, 2, 1, 8, 9]
 
 end
 
